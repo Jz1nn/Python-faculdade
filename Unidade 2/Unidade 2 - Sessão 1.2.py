d23f1d2a135f9ff449d17680ff007e3eb7c46086
Jz1nn <jz1nnwln@gmail.com>
2023-03-16 19:59:21 -0300

    Corrigido erro no enunciado

--- Unidade 2/Unidade 2 - Sessão 1.2.py
+++ Unidade 2/Unidade 2 - Sessão 1.2.py
@@ -30,13 +30,13 @@ dados_1 = {
   'enviado': [False, False, False, False, False, False, False, True, False, False]
 }
 
-# RESOLUÇÃO:
-
 dados_2 = {
   'nome': ['Travis Shepherd', 'Hoyt Glass', 'Jennifer Aguirre', 'Cassady Ayers', 'Colin Myers', 'Herrod Curtis', 'Cecilia Park', 'Hop Byrd', 'Beatrice Silva', 'Alden Morales'],
   'email': ['at@sed.org', 'ac.arcu.Nunc@auctor.edu', 'nunc.Quisque.ornare@nibhAliquam.co.uk', 'non.arcu@mauriseu.com', 'fringilla.cursus.purus@erategetipsum.ca', 'Fusce.fermentum@tellus.co.uk', 'dolor.tempus.non@ipsum.net', 'blandit.congue.In@libero.com', 'nec.tempus.mauris@Suspendisse.com', 'felis@urnaconvalliserat.org'],
   'enviado': [False, False, False, True, True, True, False, True, True, False]
 }
+
+# RESOLUÇÃO:
 
 def extrair_lista_email(dict_1, dict_2):
     lista_1 = list(zip(dict_1['nome'], dict_1['email'], dict_1['enviado']))
