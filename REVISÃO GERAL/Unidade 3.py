070abab8146402bda7c72b1a1eef25eeb33f6668
Jz1nn <jz1nnwln@gmail.com>
2023-03-27 15:50:15 -0300

    Finalizado desafio 3 e a Unidade

--- REVISÃO GERAL/Unidade 3.py
+++ REVISÃO GERAL/Unidade 3.py
@@ -1177,3 +1177,218 @@ for schema in cursor.fetchall():
     #     cep VARCHAR(9) NOT NULL,
     #     data_cadastro DATE NOT NULL
     # )
+
+    
+# DESAFIO
+
+# CRUD é o acronimo para as quatro operacoes DML possiveis em uma tabela de um banco de dados. CREATE = inserir, READ = ler, UPDATE
+
+# Enunciado
+# Hoje é comum encontrar nas empresas equipes totalmente focadas em soluções que envolvem dados. Parte do time é responsável por construir componentes (softwares ou microsserviços) voltados às necessidades de armazenamento e recuperação de dados. Esses componentes precisam ser parametrizáveis, o que significa que o componente deve ser capaz de fazer a mesma tarefa para diferentes tecnologias. Para os casos de recuperação de dados em banco de dados relacional, construir um componente parametrizável significa recuperar dados de diferentes RDBMS com o mesmo código mas com diferentes parâmetros.
+
+# Sua função foi definida para construir uma solução parametrizável capaz de criar banco de dados na tecnologia SQLite, criar e apagar tabelas, o nome do banco, das tabelas e a DDL necessária para criar uma tabela (tudo deve ser parametrizável). Você também deve construir uma solução capaz de fazer um CRUD em um banco SQLite – veja que o componente deve funcionar para qualquer nome de banco e de tabela, ou seja, parâmetros.
+
+# As regras que lhe foram passadas são as seguintes:
+
+# Para criar uma nova base de dados, é necessário informar o nome.
+# Para criar uma nova tabela, é necessário informar o nome do banco que receberá a tabela e a DDL de criação.
+# Para excluir uma tabela, é necessário informar o nome do banco e da tabela.
+# Para inserir um novo registro em uma tabela, é preciso informar: o nome do banco e da tabela e um dicionário contendo a chave e o valor a ser inserido. Por exemplo, {'nome': 'João', 'idade': 30}.
+# Para recuperar, os dados é preciso informar o nome do banco e da tabela.
+# Para atualizar um novo registro em uma tabela é preciso informar: o nome do banco e da tabela e dois dicionários, um contendo a chave e o valor a ser atualizado e outro contendo a chave e o valor da condição do registro que será atualizado.
+# Para excluir um registro em uma tabela, é preciso informar: o nome do banco e da tabela e um dicionário contendo a chave e o valor da condição para localizar o registro a ser inserido. Por exemplo, {'id_cliente': 10}.
+
+
+# RESOLUCAO
+# Primeiro sera criada a classe DDL para criar o banco, criar e apagar a tabela. Entao sera criada a classe DML para as operacoes CRUD.
+# Na classe 'DDLSQLite' sera criado um METODO privado que retorna uma instancia de conexao com um banco PARAMETRIZAVEL.
+# Haverao 3 metodos publicos, um para criar um banco de dados, outro para criar a tabela e outro para apagar uma tabela. Todos os metodos sao parametrizaveis.
+# O metodo 'criar_banco_de_dados()' cria um banco fazendo uma conexao e em seguida fecha.
+
+import sqlite3
+
+class DDLSQLite:
+    def _conectar(self, nome_banco):
+        nome_banco += '.db'
+        conn = sqlite3.connect(nome_banco)
+        return conn
+    
+    def criar_banco_de_dados(self, nome_banco):
+        nome_banco += '.db'
+        sqlite3.connect(nome_banco).close()
+        print(f"O banco de dados {nome_banco} foi criado com sucesso!")
+        return None
+    
+    def criar_tabela(self, nome_banco, dll_create):
+        conn = self._conectar(nome_banco)
+        cursor = conn.cursor()
+        cursor.execute(ddl_create)
+        cursor.close()
+        conn.close()
+        print("Tabela criada com sucesso!")
+        return None
+    
+    def apagar_tabela(self, nome_banco, tabela):
+        conn = self._conectar(nome_banco)
+        cursor = conn.cursor()
+        cursor.execute(f"DROP TABLE {tabela}")
+        cursor.close()
+        conn.close()
+        print(f"A tabela {tabela} foi excluida com sucesso!")
+        return None
+    
+# Todas as operacoes CRUD precisam receber como parametro o nome do banco de dados, entao foi passado o parametro no metodo construtor. Para instanciar essa classe, deve ser informado o nome do banco e apartir do objeto, chamar os metodos e passar os demais parametros.
+
+# O nome do banco sera usado pelo metodo privado '_conectar()' que retorna uma instancia de conexao, e sera chamado internamente pelos demais metodos. O metodo 'inserir_registros' extrai como uma tupla as colunas e valores a serem usados na construcao do comando de insercao.
+# O metodo 'ler_registros()' seleciona todos os dados de uma tabela e os retorna (o retorno sera uma lista de tuplas). O metodo 'atualizar_registro()' precisa dos dados a serem atualizados (dicionario) e dos dados que serao usados para construir a condicao, sendo que a atualizacao so pode ser feita se o valor da condicao for inteiro.
+# O metodo 'apagar_registro' tambem so pode ser executado se o valor da condicao for inteiro.
+
+import sqlite3
+
+class CrudSQLite:
+    def __init__(self, nome_banco):
+        self.nome_banco = nome_banco + '.db'
+
+    def _conectar(self):
+        conn = sqlite3.connect(self.nome_banco)
+        return conn
+    
+    def inserir_registro(self, tabela, registro):
+        colunas = tuple(registro.keys())
+        valores = tuple(registro.values())
+
+        conn = self._conectar()
+        cursor = conn.cursor()
+        query = f"""INSERT INTO {tabela} {colunas} VALUES {valores}"""
+        cursor.execute(query)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        print("Dados inseridos com sucesso!")
+        return None
+    
+    def ler_registros(self, tabela):
+        conn = self._conectar()
+        cursor = conn.cursor()
+        query = f"""SELECT * FROM {tabela}"""
+        cursor.execute(query)
+        resultado = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return resultado
+    
+    def atualizar_registro(self, tabela, dado, condicao):
+        campo_alterar = list(dado.keys())[0]
+        valor_alterar = dado.get(campo_alterar)
+        campo_condicao = list(condicao.keys())[0]
+        valor_condicao = condicao.get(campo_condicao)
+
+        conn = self._conectar()
+        cursor = conn.cursor()
+        query = f"""UPDATE {tabela} SET {campo_alterar} = '{valor_alterar}' WHERE {campo_condicao} = {valor_condicao}"""
+        cursor.execute(query)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        print("Dado atualizado com sucesso!")
+        return None
+    
+    def apagar_registro(self, tabela, condicao):
+        campo_condicao = list(condicao.keys())[0]
+        valor_condicao = condicao.get(campo_condicao)
+        conn = self._conectar()
+        cursor = conn.cursor()
+        query = f"""DELETE FROM {tabela} WHERE {campo_condicao} = {valor_condicao}"""
+        cursor.execute(query)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        print("Dado excluido com sucesso!")
+        return None
+    
+# TESTES
+# Teste classe DDLSQLite
+
+# instancia objeto
+objeto_ddl = DDLSQLite()
+
+# criar banco de dados
+objeto_ddl.criar_banco_de_dados('desafio')
+
+# criar tabela chamada cliente
+ddl_create = """
+CREATE TABLE cliente ( 
+    id_cliente INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    NOME_CLIENTE TEXT NOT NULL,
+    cpf VARCHAR(14) NOT NULL,
+    email TEXT NOT NULL,
+    telefone VARCHAR(15),
+    cidade TEXT,
+    estado VARCHAR(2) NOT NULL,
+    data_cadastro DATE NOT NULL
+);
+"""
+objeto_ddl.criar_tabela(nome_banco='desafio', ddl_create=ddl_create)
+
+# excluir a tabela
+# objeto_ddl.apagar_tabela(nome_banco='desafio', tabela='cliente')
+    # O banco de dados desafio.db foi criado com sucesso!
+    # Tabela criada com sucesso!
+
+# Teste DO CRUD
+
+#inserir registros e consultar para checar se deu certo
+objeto_dml = CrudSQLite(nome_banco='desafio')
+
+# inserir registros
+dados = [
+    {
+     'nome_cliente': 'João',
+        'cpf': '111.111.111-11',
+        'email': 'joao@servidor',
+        'cidade': 'São Paulo',
+        'estado': 'SP',
+        'data_cadastro': '2020-01-01'
+    },
+    {
+      'nome_cliente': 'Maria',
+        'cpf': '222.222.222-22',
+        'email': 'maria@servidor',
+        'cidade': 'São Paulo',
+        'estado': 'SP',
+        'data_cadastro': '2020-01-01'
+    },
+]
+# para cada dicionario na lista de dados, invocar um metodo de insercao
+for valor in dados:
+    objeto_dml.inserir_registro(tabela='cliente', registro=valor)
+
+# carrega dados salvos
+dados_carregados = objeto_dml.ler_registros(tabela='cliente')
+for dado in dados_carregados:
+    print(dados)
+    # Dados inseridos com sucesso!
+    # Dados inseridos com sucesso!
+    # (1, 'João', '111.111.111-11', 'joao@servidor', None, 'São Paulo', 'SP', '2020-01-01')
+    # (2, 'Maria', '222.222.222-22', 'maria@servidor', None, 'São Paulo', 'SP', '2020-01-01')
+
+# atualizar um registro e excluir outro:
+
+# atualizar registro
+dado_atualizar = {'telefone': '(11)1.1111-1111'}
+condicao = {'id_cliente': 1}
+
+objeto_dml.atualizar_registro(tabela='cliente', dado=dado_atualizar, condicao=condicao)
+
+dados_carregados = objeto_dml.ler_registros(tabela='cliente')
+for dado in dados_carregados:
+    print(dado)
+
+# apagar registro
+condicao = {'id_cliente': 1}
+
+objeto_dml.apagar_registro(tabela='cliente', condicao=condicao)
+    # Dado atualizado com sucesso!
+    # (1, 'João', '111.111.111-11', 'joao@servidor', '(11)1.1111-1111', 'São Paulo', 'SP', '2020-01-01')
+    # (2, 'Maria', '222.222.222-22', 'maria@servidor', None, 'São Paulo', 'SP', '2020-01-01')
+    # Dado excluído com sucesso!
